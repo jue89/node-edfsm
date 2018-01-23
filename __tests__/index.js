2417b07c99947a86eb8f9f06f3ec192f581605ef
Jue <me@jue.yt>
2018-01-23 22:13:51 +0100

    Enhanced debug message
    May it has been prepared to be compatible with node-systemd-journald ;)

--- __tests__/index.js
+++ __tests__/index.js
@@ -47,14 +47,20 @@ test('warn about not consumed events', () => {
 	e.emit.mockReturnValueOnce(false);
 	const warn = jest.fn();
 	const fsm = FSM({
+		fsmName: 'testFSM',
 		firstState: 'test',
 		output: e,
 		log: { warn }
 	}).state('test', (ctx, i, o) => {
-		o('test');
+		o('testEvent');
 	});
 	fsm.run();
-	expect(warn.mock.calls[0][0]).toEqual(`Event test had no listeners`);
+	expect(warn.mock.calls[0][0]).toEqual(`testFSM: Event testEvent had no listeners`);
+	expect(warn.mock.calls[0][1]).toMatchObject({
+		message_id: 'c84984c1816e4bf7b552dd7e638e9fa9',
+		fsm_name: 'testFSM',
+		event: 'testEvent'
+	});
 });
 
 test('head over to next state', (done) => {
@@ -120,4 +126,56 @@ test('expose next handler', (done) => {
 		done();
 	});
 	fsm.run().next(null);
+});
+
+test('debug log fsm construction', () => {
+	const debug = jest.fn();
+	const fsm = FSM({
+		fsmName: 'testFSM',
+		firstState: 'test',
+		log: { debug }
+	}).state('test', () => {});
+	fsm.run();
+	expect(debug.mock.calls[0][0]).toEqual(`testFSM: Created new instance`);
+	expect(debug.mock.calls[0][1]).toMatchObject({
+		message_id: 'fdd14aefc01c4ca8a34bde4cc8f3ede4',
+		fsm_name: 'testFSM'
+	});
+});
+
+test('debug log fsm state enter', () => {
+	const debug = jest.fn();
+	const fsm = FSM({
+		fsmName: 'testFSM',
+		firstState: 'test',
+		log: { debug }
+	}).state('test', () => {});
+	fsm.run();
+	expect(debug.mock.calls[1][0]).toEqual(`testFSM: Enter state test`);
+	expect(debug.mock.calls[1][1]).toMatchObject({
+		message_id: '4d1314823a494567ba0c24dd74a8285a',
+		fsm_name: 'testFSM'
+	});
+});
+
+test('debug log fsm destruction', (done) => {
+	const debug = jest.fn();
+	const fsm = FSM({
+		fsmName: 'testFSM',
+		firstState: 'test',
+		log: { debug }
+	}).state('test', (ctx, i, o, next) => {
+		next(null);
+	}).final(() => {});
+	fsm.run();
+	setImmediate(() => {
+		try {
+			expect(debug.mock.calls[2][0]).toEqual(`testFSM: Removed instance`);
+			expect(debug.mock.calls[2][1]).toMatchObject({
+				message_id: '7be6d26c828240a0bb82fc84e5d6a662',
+				fsm_name: 'testFSM'
+			});
+			done();
+		} catch (e) { done(e); }
+	});
 });
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+let instanceCnt = 0;
+
 function FSMInstance (fsm, ctx) {
+	this.id = instanceCnt++;
 	this.input = fsm.input;
 	this.output = fsm.output;
 	this.states = fsm.states;
 	this.finalHandler = fsm.finalHandler;
+	this.fsmName = fsm.fsmName;
 	this.log = fsm.log;
 	this.ctx = ctx;
+	this.msg(this.log.debug, 'Created new instance', {
+		message_id: 'fdd14aefc01c4ca8a34bde4cc8f3ede4'
+	});
 	this.goto(fsm.firstState);
 }
 
@@ -19,7 +26,12 @@ FSMInstance.prototype.goto = function (stateName) {
 	// Expose output bus
 	const o = (name, arg) => {
 		const consumed = this.output.emit(name, arg);
-		if (!consumed) this.log.warn(`Event ${name} had no listeners`);
+		if (!consumed) {
+			this.msg(this.log.warn, `Event ${name} had no listeners`, {
+				event: name,
+				message_id: 'c84984c1816e4bf7b552dd7e638e9fa9'
+			});
+		}
 	};
 
 	// Promise waits for next state
@@ -28,6 +40,10 @@ FSMInstance.prototype.goto = function (stateName) {
 	this.next.timeout = (msecs, nextState) => {
 		toHandle = setTimeout(() => this.next(nextState), msecs);
 	};
+	this.msg(this.log.debug, `Enter state ${stateName}`, {
+		message_id: '4d1314823a494567ba0c24dd74a8285a',
+		state: stateName
+	});
 	this.states[stateName](this.ctx, i, o, this.next);
 	leave.then((nextState) => {
 		if (toHandle) clearTimeout(toHandle);
@@ -39,14 +55,29 @@ FSMInstance.prototype.goto = function (stateName) {
 
 FSMInstance.prototype.leave = function () {
 	this.finalHandler();
+	this.msg(this.log.debug, 'Removed instance', {
+		message_id: '7be6d26c828240a0bb82fc84e5d6a662'
+	});
+};
+
+FSMInstance.prototype.msg = function (handler, msg, info) {
+	if (!handler) return;
+	if (this.fsmName) {
+		info.fsm_name = this.fsmName;
+		msg = `${this.fsmName}: ${msg}`;
+	}
+	info.fsm_id = this.id;
+	handler(msg, info);
 };
 
 function FSM (opts) {
 	this.firstState = opts.firstState;
 	this.input = opts.input;
 	this.output = opts.output;
+	this.fsmName = opts.fsmName;
 	this.log = {
-		warn: opts.log && opts.log.warn ? opts.log.warn : () => {}
+		debug: opts.log && opts.log.debug ? opts.log.debug : undefined,
+		warn: opts.log && opts.log.warn ? opts.log.warn : undefined
 	};
 	this.finalHandler = () => {};
 	this.states = {};
